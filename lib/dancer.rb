f3e6a00b5312f689be5bf13e75164c3ca20f4b7d
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-03 06:56:06 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,9 +2,9 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
   
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-  
+  # extend FancyDance::ClassMethods
+  # include FancyDance::InstanceMethods
+  include Dance
   attr_accessor :name
  
   def initialize(name)
